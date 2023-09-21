a77f8da6567b7daa6989e714f392a0da8c36713d
niz33 <97012971+niz33@users.noreply.github.com>
2023-09-21 12:29:08 -0400

    Update BMI.java

--- BMI.java
+++ BMI.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 import java.io.*;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -19,14 +20,14 @@ public class BMI {
     public static void main(String[] args) {
         System.out.println("============BMI CALCULATOR============");
         System.out.println("Do you want to use imperial measureents instead of metric? (Y/N)");
-        usingImperial=(scanner.nextLine()=="Y");
+        usingImperial=(Objects.equals(scanner.nextLine(), "Y"));
         System.out.println("Please input your height (in meters or feet) followed by your weight (in kilograms or pounds):");
-        height=scanner.nextInt();
-        weight=scanner.nextInt();
+        height=scanner.nextDouble();
+        weight=scanner.nextDouble();
         if(usingImperial){
             convert_to_metric();
         }
-        System.out.println("You BMI value is "+weight/height/height+".");
+        System.out.println("You BMI value is "+(BMI=weight/height/height)+".");
         if(BMI<18.5){
             System.out.println("You are underweight!");
         }
